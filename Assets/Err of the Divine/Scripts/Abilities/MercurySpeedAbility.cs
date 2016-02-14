ecf952fa774617128676a3e694db0104496d21b2
Kitty Liang <lkitty491@yahoo.com>
2016-02-13 21:33:01 -0800

    Added MercurySpeedAbility.cs Changed walk and run speeds to public so I could change them for the ability.

---
+++ Assets/Err of the Divine/Scripts/Abilities/MercurySpeedAbility.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityStandardAssets.Characters.FirstPerson{
+
+	public class MercurySpeedAbility : MonoBehaviour {
+
+		private FirstPersonController controller;
+
+		void Start () {
+			controller = GetComponent<FirstPersonController>();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetKeyDown(KeyCode.F)){
+				// Yuima goes fast
+				controller.m_RunSpeed = 50f;
+				controller.m_WalkSpeed = 50f;
+				Invoke("revertSpeed", 0.3f);
+			}
+		}
+
+		void revertSpeed(){
+			controller.m_RunSpeed = 10f;
+			controller.m_WalkSpeed = 5f;
+		}
+	}
+}
