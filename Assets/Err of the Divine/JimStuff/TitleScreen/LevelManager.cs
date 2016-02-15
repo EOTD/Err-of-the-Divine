a415934c97ab8e23abe7378d8b495ae1ee97edc6
jkim1987 <buhssuht@hotmail.com>
2016-02-15 15:59:45 -0800

    Title Screen. Temp and Script

---
+++ Assets/Err of the Divine/JimStuff/TitleScreen/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLevel(string name)
+	{
+		Debug.Log("Level Load reqquested for: " +name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest()
+	{
+		Debug.Log("I want to quit");
+		Application.Quit();
+	}
+}
